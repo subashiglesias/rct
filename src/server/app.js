ea0b8775ec74cd3f37a4f88bab236a64435a594a
Subash P <subash.p@mrcooper.com>
2021-01-26 16:58:53 +0530

    Added Mongo local

--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/consTool", { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', () => console.error('Connection error!'));
+db.once('open', () => {
+    console.info('Mongo Successfully connected!');
+});
+
+
 const express = require("express"),
     app = express(),
     port = process.env.PORT || 5000,
