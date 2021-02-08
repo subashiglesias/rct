f0b182afa8c36356345ace3f74faa2f71f28c4bf
Subash P <subash.p@mrcooper.com>
2021-02-08 16:53:43 +0530

    Small header redirect fix

--- src/client/components/AppInfo/AppInfo.jsx
+++ src/client/components/AppInfo/AppInfo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './AppInfo.css'
+import config from './../../../server/config'
+import {RouterPaths} from "../../constants";
 
 const Appinfo = () => {
     return (
         <div className="App-header">
-            <h3>Construction Tool</h3>
+            <h3 onClick={() => window.location.href = `${config.appRoute}${RouterPaths.ROOT}`}>Construction Tool</h3>
         </div>
     )
 }
